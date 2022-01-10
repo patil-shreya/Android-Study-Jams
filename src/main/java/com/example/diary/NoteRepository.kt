4ab8208063f4e9c5049fb074c7d1e1b1e1a920cc
Shreya Patil <89247835+patil-shreya@users.noreply.github.com>
2022-01-10 12:04:36 +0530

    Add files via upload

---
+++ src/main/java/com/example/diary/NoteRepository.kt
@@ -0,0 +1,22 @@
+package com.example.diary
+
+import androidx.annotation.WorkerThread
+import androidx.lifecycle.LiveData
+import java.util.concurrent.Flow
+
+class NoteRepository(private val notesDao: NotesDao) {
+
+    val allNotes: LiveData<List<Note>> = notesDao.getAllNotes()
+
+    suspend fun insert(note: Note) {
+        notesDao.insert(note)
+    }
+
+    suspend fun delete(note: Note){
+        notesDao.delete(note)
+    }
+
+    suspend fun update(note: Note){
+        notesDao.update(note)
+    }
+}
